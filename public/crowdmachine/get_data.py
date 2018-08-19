1987af39245186e98a07920e8a48561a290a19b1
Raphaël Monnard <raphael@monnard.email>
2018-08-19 21:37:23 +0200

    update

--- public/crowdmachine/get_data.py
+++ public/crowdmachine/get_data.py
@@ -10,7 +10,7 @@ def to_base64(file):
         return base64.b64encode(image_file.read()).decode('ascii')  
 
 url = 'https://docs.google.com/spreadsheets/d/1uS8elJvel5WN_dvNN7ziWpwc-Qz3lVh0GdPCYTN90-s/gviz/tq?tqx=out:csv&sheet='
-df = pd.read_csv(url)[['sort', 'id', 'title', 'description', 'specification']]
+df = pd.read_csv(url)[['sort', 'id', 'get',  'title', 'description', 'specification']]
 
 df['image'] = df.apply(lambda row: to_base64(os.path.join(path, str(row.id) + '_400x400.png')), axis=1)
 df['specification'] = df.apply(lambda row: row['specification'].split('\n'), axis=1)
