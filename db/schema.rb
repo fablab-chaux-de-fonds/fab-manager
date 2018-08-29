a086eeacc4a69872220b5378dcdca9e866cb416d
ioknox <samuel.nussbaum@erobot.ch>
2018-08-29 21:22:01 +0200

    missing schema model on reservable relation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180825171202) do
+ActiveRecord::Schema.define(version: 20180826201356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -285,6 +285,7 @@ ActiveRecord::Schema.define(version: 20180825171202) do
     t.datetime "updated_at"
     t.string   "slug"
     t.boolean  "disabled"
+    t.boolean  "reservable"
   end
 
   add_index "machines", ["slug"], name: "index_machines_on_slug", unique: true, using: :btree
